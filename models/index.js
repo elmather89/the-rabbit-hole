4d043246282794072b1cf9167ea69978f7ff7c9b
CKayV11 <11303975+ckayv11@users.noreply.github.com>
2019-07-11 21:41:08 -0500

    added code to export object containing all models

---
+++ models/index.js
@@ -0,0 +1,8 @@
+// Exporting an object containing all of our models
+
+module.exports = {
+    Book: require("./Book"),
+    Author: require("./Author"),
+    AuthorCatalog: require("./AuthorCatalog"),
+    BookCatalog: require("./BookCatalog")
+  };
