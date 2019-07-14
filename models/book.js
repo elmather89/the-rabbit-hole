3e1ecaa6f8b6e950f7fb42b9568338f345deeeb2
CKayV11 <11303975+ckayv11@users.noreply.github.com>
2019-07-14 17:04:05 -0500

    added mongoose-thumbnail plugin to book model

--- models/book.js
+++ models/book.js
@@ -1,6 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+var thumbnailPluginLib = require('mongoose-thumbnail');
+var thumbnailPlugin = thumbnailPluginLib.thumbnailPlugin;
+var make_upload_to_model = thumbnailPluginLib.make_upload_to_model;
+ 
+var uploads_base = path.join(__dirname, "uploads");
+var uploads = path.join(uploads_base, "u");
+
 const bookSchema = new Schema({
   title: {
     type: String,
@@ -30,9 +37,9 @@ const bookSchema = new Schema({
   quote: {
     type: String,
   },
-  image: {
-    type: String,
-  },
+  // image: {
+  //   type: String,
+  // },
   bookAdded: {
     type: Date,
     default: Date.now
@@ -46,6 +53,18 @@ bookSchema.methods.lastUpdatedDate = function() {
   this.lastUpdated = Date.now();
   return this.lastUpdated;
 };
+
+// Using mongoose-thumbnail plugin to handle image file uploads
+
+bookSchema.plugin(thumbnailPlugin, {
+  name: "bookImage",
+  format: "jpg",
+  size: 80,
+  inline: false,
+  save: true,
+  upload_to: make_upload_to_model(uploads, 'bookImage'),
+  relative_to: uploads_base
+});
 
 const Book = mongoose.model("Book", bookSchema);
 
