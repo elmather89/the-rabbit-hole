8157be898f9a6dc0c245a93e0463d01157b47556
Robert Rowan <rowanb27@yahoo.com>
2019-07-15 17:13:02 -0500

    Detail.js update

---
+++ client/src/pages/Detail.js
@@ -0,0 +1,74 @@
+import React from "react";
+
+
+render() {
+    return (
+        <Container>
+            <Row>
+                <Col size="md-9">
+                <Card
+                        heading="Slobodkina, Esphyr (1908-2002)"
+                   > </Card>
+                   </Col>
+
+                   <Col size="md-3">
+                       <Card>
+                           <img class="authorImage" src="./images/Esphyr Slobodkina.jpg" alt="Esphyr Slobodkina" style="width:100px;height:100px;"></img>
+                       </Card>
+                   </Col>
+                   </Row>
+                <Row>
+                <Col size="md-12">
+                    
+                    <Card heading="Creator Biography">
+                     <div>
+                         <p>
+                             Esphyr Slobodkina is a celebrated abstract 
+                             and author of the children's classic Caps 
+                             for Sale. A founding member of the influential 
+                             American Abstract Artists group in 1936, 
+                             Slobodkina helped pave the way for the 
+                             acceptance of abstract art in the United 
+                             States. She was also a childrens' book author,
+                             illustrator, first collaborating with Margaret 
+                             Wise Brown on several stories before publishing 
+                             Caps for Sale in 1940.
+                         </p>
+                         </div>
+                    </Card>
+                </Col>
+                </Row>
+                
+                <Row>
+                <Col size="md-6">
+                    <Card heading="Legacy">
+
+                        <div>
+                            <p>First to use collage in children's books.
+                                 Pioneer of early of American abstraction.
+                                 At age 91, established the Slobodkina
+                                 Foundation, dedicated to the consservation,
+                                    preservation, and exhibition of art.</p>
+
+                        </div>
+                       
+                     </Card>
+                </Col>
+                <Col size="md-6">
+                    <Card heading="Own Words">
+
+                        <div>
+                            <p>"The verbal patterns and the patterns 
+                                of behavior we present to children in 
+                                these lighthearted confections are 
+                                likely to influence them for the rest 
+                                of their lives. These asthetic impressions,
+                                just like the moral teachings of early 
+                                childhood, remain indelible." </p>
+                        
+                        </div>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    )}
