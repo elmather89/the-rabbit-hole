dd244273097a11badf038f155ce2967b1da09331
Robert Rowan <rowanb27@yahoo.com>
2019-07-15 21:55:35 -0500

    added detail route

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+
 import './App.css';
 
 function App() {
@@ -12,8 +15,10 @@ function App() {
         <Switch>
           <Route exact path="/" component={Books} />
           <Route exact path="/books" component={Books} />
-          {/* <Route exact path="/books/:id" component={Detail} />
-          <Route component={NoMatch} /> */}
+          <Route exact path="/books/:id" component={Detail} />
+          <Route exact path="/detail" component={Detail} />
+          
+          <Route component={NoMatch} /> 
         </Switch>
       </div>
     </Router>
