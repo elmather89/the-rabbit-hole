46e185a8532433f40b2ef8c3ff06176d18f469ec
Sansevieria <elizabethleighmather@gmail.com>
2019-07-18 16:06:28 -0500

    routes issues

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
 const bookRoutes = require("./booksRoutes");
-
-// const authRoutes = require('./auth');
-// router.use('/auth', authRoutes);
+// const creatorRoutes = require("./creatorRoutes");
+// const userRoutes = require("./user");
 
 // Book routes
 router.use("/books", bookRoutes);
+
+// Creator routes
+// router.use("/creator", creatorRoutes);
+
+// User routes
+// router.use("/users", userRoutes);
 
 module.exports = router;
