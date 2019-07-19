b2c37b4b316be2bdf32cd29e97aac176f1e4982e
CKayV11 <11303975+ckayv11@users.noreply.github.com>
2019-07-18 23:11:08 -0500

    updated bookcard code to include children

--- client/src/pages/bookDetails.js
+++ client/src/pages/bookDetails.js
@@ -4,32 +4,56 @@ import BookHeader from "../components/BookHeader";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import API from "../utils/API";
+import headerLogo from "../assets/images/100year.jpg";
 
 class bookDetails extends Component {
     state = {
         book: {},
-        creator: {},
+        creator: {}
     };
-// When this component mounts, grab the book with the _id of this.props.match.params.id
+
     componentDidMount() {
+      this.loadBookDetails();
+      // this.loadCreatorDetails();
+  };
+
+// When this component mounts, grab the book with the _id of this.props.match.params.id
+    loadBookDetails = () => {
         API.getBook(this.props.match.params.id)
           .then(res => this.setState({ book: res.data }))
           .catch(err => console.log(err));
     };
 
+
+    // loadCreatorDetails = () => {
+    //   API.getCreator(this.props.match.params.id)
+    //       .then(res =>
+    //           this.setState({ creator: res.data, }))
+    //        .catch(err => console.log(err));
+    // };
+
     render() {
         return (
           <Container fluid>
               <Row>
-                <Col size="md-12">
+                <Col size="sm-12">
                   <BookHeader>
-                    <h1>
-                        {this.state.book.title}
-                    </h1>
-                    <h3>
-                        {this.state.book.creator}
-                    </h3>
-                    <p>({this.state.creator.birthdate} - {this.state.creator.dateOfDeath})</p>
+                    <Row>
+                    <Col size="sm-9">
+                      <h1>
+                          {this.state.book.title}
+                      </h1>
+                      <h3>
+                          By {this.state.book.creator}
+                      </h3>
+                      <p>({this.state.creator.birthdate} - {this.state.creator.dateOfDeath})</p>
+                    </Col>
+                    <Col size="sm-3">
+                      <div>
+                        <img src={headerLogo} alt="100YearPanorama"></img>
+                      </div>
+                    </Col>
+                    </Row>
                   </BookHeader>
                 </Col>
               </Row>
