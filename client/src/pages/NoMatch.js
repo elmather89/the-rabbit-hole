7662dca93b65d04ac2d3498135226c30ec209f9b
CKayV11 <11303975+ckayv11@users.noreply.github.com>
2019-07-18 19:43:20 -0500

    added link back to Homepage on 404

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
 import logo from "../assets/images/jackRabbit.gif";
+import { Link } from "react-router-dom";
 
 function NoMatch() {
     return (
@@ -10,6 +11,7 @@ function NoMatch() {
                 <Col size="md-12">
                     <Jumbotron>
                         <h1>404 Page Not Found</h1>
+                        <Link to="/">← Back to Homepage</Link>
                     </Jumbotron>
                         <img src= {logo} alt="jackrabbit" fluid />
                 </Col>
