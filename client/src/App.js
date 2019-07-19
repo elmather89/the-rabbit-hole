e6ac20a505a3ecdbd0b0e1d48037a40761d077b5
CKayV11 <11303975+ckayv11@users.noreply.github.com>
2019-07-18 20:11:39 -0500

    modified books/:id route

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ export function App() {
         <Switch>
           <Route exact path="/" component={Books} />
           <Route exact path="/books" component={Books} />
-          <Route exact path="/books/:id" component={creatorDetails} />
+          <Route exact path="/books/:id" component={bookDetails} />
           <Route exact path="/creator/:id" component={creatorDetails} />
           <Route component={NoMatch} /> 
         </Switch>
