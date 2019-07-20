9ad3c970c44492d989cba198e6430fd4af740e0e
Robert Rowan <rowanb27@yahoo.com>
2019-07-19 21:33:18 -0500

    Updated legacy & ownWords

--- client/src/pages/creatorDetails.js
+++ client/src/pages/creatorDetails.js
@@ -67,16 +67,7 @@ class creatorDetails extends Component {
 
                         <div>
                             <p>
-                                Esphyr Slobodkina is a celebrated abstract
-                                and author of the children's classic Caps
-                                for Sale. A founding member of the influential
-                                American Abstract Artists group in 1936,
-                                Slobodkina helped pave the way for the
-                                acceptance of abstract art in the United
-                                States. She was also a childrens' book author,
-                                illustrator, first collaborating with Margaret
-                                Wise Brown on several stories before publishing
-                                Caps for Sale in 1940.
+                                {this.state.creator.biography}
                          </p>
                         </div>
 
@@ -89,11 +80,7 @@ class creatorDetails extends Component {
                         <Card heading="Legacy" className="legacy">
 
                             <div>
-                                <p>First to use collage in children's books.
-                                     Pioneer of early of American abstraction.
-                                     At age 91, established the Slobodkina
-                                     Foundation, dedicated to the consservation,
-                                    preservation, and exhibition of art.</p>
+                                <p>{this.state.creator.legacy}</p>
 
                             </div>
 
@@ -103,13 +90,7 @@ class creatorDetails extends Component {
                         <Card heading="Own Words" className="ownWords">
 
                             <div>
-                                <p>"The verbal patterns and the patterns
-                                    of behavior we present to children in
-                                    these lighthearted confections are
-                                    likely to influence them for the rest
-                                    of their lives. These asthetic impressions,
-                                    just like the moral teachings of early
-                                childhood, remain indelible." </p>
+                                <p>{this.state.creator.ownWords} </p>
 
                             </div>
                         </Card>
