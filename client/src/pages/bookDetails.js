67a6de704404e7a09ec5c12ab86a9b963f2a6ce9
CKayV11 <11303975+ckayv11@users.noreply.github.com>
2019-07-22 19:12:14 -0500

    added edit book modal

--- client/src/pages/bookDetails.js
+++ client/src/pages/bookDetails.js
@@ -5,6 +5,10 @@ import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import API from "../utils/API";
 import headerLogo from "../assets/images/100year.jpg";
+// import EditBtn from "../components/EditBtn";
+import BookEditModal from "../components/BookDetailsEdit";
+import { Input, TextArea, CheckBox, FormBtn } from "../components/Form";
+import Button from "../components/Button";
 import "../assets/style/style.css";
 
 
@@ -33,6 +37,57 @@ class bookDetails extends Component {
     //        .catch(err => console.log(err));
     // };
 
+    openBookEditModalHandler = () => {
+      this.setState({
+          isShowingBook: true,
+          isShowingCreator: false,
+          modalDivClass: true
+      });
+    };
+
+    closeBookEditModalHandler = () => {
+      this.setState({
+          isShowingBook: false,
+          modalDivClass: false
+      });
+    }
+
+    updateBook = id => {
+      API.updateBook(id)
+          .then(res => this.loadBookDetails())
+          .catch(err => console.log(err));
+    };
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleBookEditSubmit = event => {
+        event.preventDefault();
+        if (this.state.title) {
+            API.updateBook({
+                title: this.state.title,
+                creator: this.state.creator,
+                birthdate: this.state.birthdate,
+                dateOfDeath: this.state.dateOfDeath,
+                tags: this.state.tags,
+                accomplishments: this.state.accomplishments,
+                biography: this.state.biography,
+                quote: this.state.quote,
+                synopsis: this.state.synopsis,
+                originalPublisher: this.state.originalPublisher,
+                currentPublisher: this.state.currentPublisher,
+                yearPublished: this.state.yearPublished,
+                bookImage: this.state.bookImage
+            })
+                .then(res => this.loadBookDetails())
+                .catch(err => console.log(err));
+        }
+    };
+
     render() {
         return (
           <Container fluid>
@@ -45,6 +100,10 @@ class bookDetails extends Component {
                       <h3>By {this.state.book.creator}</h3>
                       <p>({this.state.book.birthdate} - {this.state.book.dateOfDeath})</p>
                       <hr></hr>
+                      {/* <small><EditBtn onClick={() => this.updateBook(this.state.book._id)} /></small> */}
+                      <Button className="open-modal-btn" onClick={this.openBookEditModalHandler}>
+                            Edit Details
+                      </Button>
                       <p>{this.state.book.tags}</p>
                       <p>{this.state.book.biography}</p>
                     </Col>
@@ -54,6 +113,14 @@ class bookDetails extends Component {
                       </div>
                     </Col>
                     </Row>
+                    {/* <Row className="headerRow">
+                    <Col size="sm-12">
+                      <small><EditBtn onClick={() => this.updateBook(this.state.book._id)} /></small>
+                      <hr></hr>
+                      <p>{this.state.book.tags}</p>
+                      <p>{this.state.book.biography}</p>
+                    </Col>
+                    </Row> */}
                   </BookHeader>
                 </Col>
               </Row>
@@ -97,6 +164,108 @@ class bookDetails extends Component {
                     </Row>
                   </BookCard>
                 </Col>
+              </Row>
+
+              <Row>
+                <div className={!this.state.isShowingBook ? "hideModalDiv" : 'showModalDiv'}>
+                  <BookEditModal
+                      className="modal"
+                      show={this.state.isShowingBook}
+                      close={this.closeBookEditModalHandler}>
+                      <form>
+                          <p className="form-label"><small>Title</small></p>
+                          <Input
+                              value={this.state.book.title}
+                              onChange={this.handleInputChange}
+                              name="title"
+                              placeholder="Book Title (required)"
+                          />
+                          <p className="form-label"><small>Creator Name</small></p>
+                          <Input
+                              value={this.state.book.creator}
+                              onChange={this.handleInputChange}
+                              name="creator"
+                              placeholder="Creator (required)"
+                          />
+                          <p className="form-label"><small>Year Born</small></p>
+                          <Input
+                              value={this.state.birthdate}
+                              onChange={this.handleInputChange}
+                              name="birthdate"
+                              placeholder="YYYY"
+                          />
+                          <p className="form-label"><small>Year Passed</small></p>
+                          <Input
+                              value={this.state.dateOfDeath}
+                              onChange={this.handleInputChange}
+                              name="dateOfDeath"
+                              placeholder="YYYY (if applicable)"
+                          />
+                          <p className="form-label"><small>Occupation(s)</small></p>
+                          <TextArea
+                              value={this.state.tags}
+                              onChange={this.handleInputChange}
+                              name="tags"
+                              placeholder="Author / Illustrator / Painter / etc."
+                          />
+                          <p className="form-label"><small>Biography</small></p>
+                          <TextArea
+                              value={this.state.biography}
+                              onChange={this.handleInputChange}
+                              name="biography"
+                              placeholder="Biography"
+                          />
+                          <p className="form-label"><small>Text of Interest / Quote</small></p>
+                          <TextArea
+                              value={this.state.quote}
+                              onChange={this.handleInputChange}
+                              name="quote"
+                              placeholder="Quote"
+                          />
+                          <p className="form-label"><small>Book Synopsis</small></p>
+                          <TextArea
+                              value={this.state.synopsis}
+                              onChange={this.handleInputChange}
+                              name="synopsis"
+                              placeholder="Synopsis"
+                          />
+                          <p className="form-label"><small>Original Publisher</small></p>
+                          <Input
+                              value={this.state.originalPublisher}
+                              onChange={this.handleInputChange}
+                              name="originalPublisher"
+                              placeholder="Original Publisher"
+                          />
+                          <p className="form-label"><small>Current Publisher</small></p>
+                          <Input
+                              value={this.state.currentPublisher}
+                              onChange={this.handleInputChange}
+                              name="currentPublisher"
+                              placeholder="Current Publisher"
+                          />
+                          <p className="form-label"><small>Original Year Published</small></p>
+                          <Input
+                              value={this.state.yearPublished}
+                              onChange={this.handleInputChange}
+                              name="yearPublished"
+                              placeholder="YYYY"
+                          />
+                          <p className="form-label"><small>Book Cover Image</small></p>
+                          <Input
+                              value={this.state.bookImage}
+                              onChange={this.handleInputChange}
+                              name="bookImage"
+                              placeholder="Enter book image URL"
+                          />
+                          <button type="button" className="btn btn-success"
+                            onClick={this.handleFormSubmit}
+                            // onClick={() => this.updateBook(this.state.book._id)}
+                          >
+                            Update Book
+                          </button>
+                      </form>
+                  </BookEditModal>
+                </div>
               </Row>
 
               <Row>
